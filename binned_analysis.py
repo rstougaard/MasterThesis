cd820ffc938c3fdcebf0046c6b0e58abbfb3d726
rstougaard <yrikke@cpcdata.dk>
2024-11-24 22:38:11 +0100

    bug fix

--- binned_analysis.py
+++ binned_analysis.py
@@ -173,7 +173,7 @@ def run_binned_likelihood(vars):
 
     # Print parameter values before fitting to help debug bounds issues
     for param in like.model[source_name].funcs['Spectrum'].params:
-        print(f"Parameter {param.getName()}: value={param.value()}, min={param.getMin()}, max={param.getMax()}")
+        print(f"Parameter { param }
     
     try:
         like.fit(verbosity=0, covar=True, optObject=likeobj)
