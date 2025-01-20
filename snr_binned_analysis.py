e13a4fc671645f6608cf0959cd2bd9acb183bf01
rstougaard <yrikke@cpcdata.dk>
2025-01-20 13:32:46 +0100

    trying to refit NONE

--- snr_binned_analysis.py
+++ snr_binned_analysis.py
@@ -1231,7 +1231,7 @@ generate_files(vars_lin, time_intervals=time_intervals, number_of_bins=7)
 source_maps(vars_lin, time_intervals=time_intervals)
 print('Generated all files for Likelihood!')
 
-run_binned_likelihood(vars_none, free_params = "None")
+run_binned_likelihood(vars_none, free_params = "alpha")
 print('Likelihood for non filtered data done!')
 '''
 run_binned_likelihood(vars_snr, snrratios=snrratios, free_params = "None")
