1da6808c45beac123fc9d2bcab5e84a39d0c23dd
rstougaard <yrikke@cpcdata.dk>
2025-03-14 11:01:54 +0100

    checking the individual heatmaps

--- what_is_going_on.py
+++ what_is_going_on.py
@@ -198,4 +198,4 @@ with open("all_results_none_32_curve_fit_nodivbin.pkl", "rb") as file:
     all_results_snr = pickle.load(file)
 
 no_filtering_sources = list(all_results_none.keys()) 
-plot_individual_delta_chi2_heatmap_with_pdf(all_results_none, no_filtering_sources, " ", filtering_methods="No_Filtering", pdf_filename="indv_heatmaps_no_filter.pdf")
+plot_individual_delta_chi2_heatmap_with_pdf(all_results_lin, no_filtering_sources, " ", filtering_methods="month", pdf_filename="indv_heatmaps_month.pdf")
