bafc50c02011cde4691c5cbb86821eaa5c19380d
rstougaard <yrikke@cpcdata.dk>
2025-03-27 11:48:15 +0100

    denys help

--- help.py
+++ help.py
@@ -13,4 +13,4 @@ with open("all_results_none_31_logpar_no_sys_error.pkl", "rb") as file:
 
 focus_source = all_results_none["4FGL J0309.4-4000"]["No_Filtering"]
 
-print(focus_source[:10])
+print(focus_source[:2])
