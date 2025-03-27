992ae284a21d1b1ad57f9cc46b36513e07d95091
rstougaard <yrikke@cpcdata.dk>
2025-03-27 11:46:19 +0100

    denys help

---
+++ help.py
@@ -0,0 +1,16 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+import shlex
+from iminuit.cost import LeastSquares
+from iminuit import Minuit
+from matplotlib.backends.backend_pdf import PdfPages
+import pickle
+
+with open("all_results_none_31_logpar_no_sys_error.pkl", "rb") as file:
+    all_results_none = pickle.load(file)
+
+focus_source = all_results_none["4FGL J0309.4-4000"]["No_Filtering"]
+
+print(focus_source[:10])
