8989dbab1019cecd21d43841bb21487842d5fab2
rstougaard <yrikke@cpcdata.dk>
2025-03-31 17:21:28 +0200

    trying my analysis on Denys data

--- snr_binned_analysis.py
+++ snr_binned_analysis.py
@@ -978,10 +978,13 @@ def run_binned_likelihood(vars, snrratios=None, time_intervals=None, free_params
                         binexpmap = prefix+str(ebin)+'_'+str(tbin)+'_BexpMap.fits'
                         ltcube = prefix+'J0617_expCube_'+str(tbin)+'.fits'
                         input_model =prefix+str(ebin)+'_'+str(tbin)+'_model_map.fits'
-                        
+
                         obs = BinnedObs(srcMaps=srcmap, binnedExpMap=binexpmap, expCube=ltcube, irfs='CALDB')
-                        like = BinnedAnalysis(obs, srcmdl=input_model, optimizer='NewMinuit')
-                        likeobj = pyLikelihood.NewMinuit(like.logLike)
+                        like = binnedAnalysis(obs, srcmdl=input_model, optimizer='NewMinuit')
+                        cfg = BinnedConfig(edisp_bins=edisp_bins)
+                        #print( 'Will launch analysis with edisp_bins=',cfg.edisp_bins() )
+                        #analysis = binnedAnalysis (config=cfg, irfs=irf,cmap=cmapfile,bexpmap=bexpmapfile,expcube=expcubefile,srcmdl=model, optimizer=optimizer)
+                        #likeobj = pyLikelihood.NewMinuit(like.logLike)
                         like.fit(verbosity=0, covar=True, optObject=likeobj)
                         TS = like.Ts(source_name) #also include in output file
                         convergence = likeobj.getRetCode()  #also include in output file
