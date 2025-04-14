d4c9a59958308fbd153f602bf993d7f6fd9f7ecb
rstougaard <yrikke@cpcdata.dk>
2025-04-14 10:59:59 +0200

    making heatsmaps for all

--- what_is_going_on.py
+++ what_is_going_on.py
@@ -588,8 +588,12 @@ no_filtering_grid = compute_mean_delta_chi2_grid(
 #Testing on sys base plotting function.. changes has to be done for the nosys plotting function 
 
 plot_mean_delta_chi2_heatmap_sys_base(None, all_results_none_sys, list(all_results_none.keys()), "base_sys_",  remove_source_label=None)
-plot_mean_delta_chi2_heatmap_sys_base(None, all_results_lin_sys, list(all_results_lin.keys()), "base_sys_", no_filtering_grid=no_filtering_grid_sys,no_filtering_grid_other=None, remove_source_label=None)
-plot_mean_delta_chi2_heatmap_sys_base(None, all_results_snr_sys, list(all_results_snr.keys()), "base_sys_", no_filtering_grid=no_filtering_grid_sys, no_filtering_grid_other=None, remove_source_label=None)
+plot_mean_delta_chi2_heatmap_sys_base(None, all_results_lin_sys, list(all_results_lin.keys()), "base_sys_", no_filtering_grid=no_filtering_grid_sys,no_filtering_grid_other=no_filtering_grid, remove_source_label=None)
+plot_mean_delta_chi2_heatmap_sys_base(None, all_results_snr_sys, list(all_results_snr.keys()), "base_sys_", no_filtering_grid=no_filtering_grid_sys, no_filtering_grid_other=no_filtering_grid, remove_source_label=None)
+
+plot_mean_delta_chi2_heatmap_nosys_base(all_results_none, None, list(all_results_none.keys()), "base_nosys_",  remove_source_label=None)
+plot_mean_delta_chi2_heatmap_nosys_base(all_results_lin, None, list(all_results_lin.keys()), "base_nosys_", no_filtering_grid=no_filtering_grid, no_filtering_grid_other=no_filtering_grid_sys, remove_source_label=None)
+plot_mean_delta_chi2_heatmap_nosys_base(all_results_snr, None, list(all_results_snr.keys()), "base_nosys_", no_filtering_grid=no_filtering_grid, no_filtering_grid_other=no_filtering_grid_sys,remove_source_label=None)
 '''
 plot_mean_delta_chi2_heatmap_nosys_base(all_results_none, all_results_none_sys, list(all_results_none.keys()), "base_nosys_",  remove_source_label=None)
 plot_mean_delta_chi2_heatmap_nosys_base(all_results_lin, all_results_lin_sys, list(all_results_lin.keys()), "base_nosys_", no_filtering_grid=no_filtering_grid, no_filtering_grid_other=no_filtering_grid_sys, remove_source_label=None)
@@ -599,7 +603,4 @@ plot_mean_delta_chi2_heatmap_sys_base(all_results_none, all_results_none_sys, li
 plot_mean_delta_chi2_heatmap_sys_base(all_results_lin, all_results_lin_sys, list(all_results_lin.keys()), "base_sys_", no_filtering_grid=no_filtering_grid_sys,no_filtering_grid_other=no_filtering_grid, remove_source_label=None)
 plot_mean_delta_chi2_heatmap_sys_base(all_results_snr, all_results_snr_sys, list(all_results_snr.keys()), "base_sys_", no_filtering_grid=no_filtering_grid_sys, no_filtering_grid_other=no_filtering_grid, remove_source_label=None)
 
-#plot_mean_delta_chi2_heatmap_limit(all_results_none, all_results_none_sys, list(all_results_none.keys()), "limit_mean",  remove_source_label=None)
-#plot_mean_delta_chi2_heatmap_limit(all_results_lin, all_results_lin_sys, list(all_results_lin.keys()), "limit_mean_", no_filtering_grid=no_filtering_grid, remove_source_label=None)
-#plot_mean_delta_chi2_heatmap_limit(all_results_snr, all_results_snr_sys, list(all_results_snr.keys()), "limit_mean_", no_filtering_grid=no_filtering_grid, remove_source_label=None)
 '''
