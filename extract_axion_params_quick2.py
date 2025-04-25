19ffaf00ee649a2673f52e87d5b5ce8cf7f201d5
rstougaard <yrikke@cpcdata.dk>
2025-04-25 20:24:18 +0200

    fitting NONE and LIN

--- extract_axion_params_quick2.py
+++ extract_axion_params_quick2.py
@@ -41,7 +41,7 @@ mass_unique = axion_data[::n_g, 0]     # length = n_mass
 
 # Define your desired start and stop values.
 m_start_val = 1e-10
-g_start_val = 7.5e-13
+g_start_val = 5e-13
 m_stop_val  = 1.5e-8
 g_stop_val  = 1e-11
 
